6d40461520f254c033ad2f6d5d7137b23e5ba7f5
Romain niam0r <romain@niamor.me>
2020-01-31 12:21:33 +0000

    Add nokogiri gem and implement scraper in seeds.rb
    TODO: extract scraper logic

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+Quote.destroy_all
+puts "Destroyed all quotes"
+
+url = "https://www.goodreads.com/quotes/list/61714489-romain-niam0r?utf8=%E2%9C%93&sort=date_added"
+# url = "./app/my_quotes.html"
+doc = Nokogiri::HTML(open(url), nil, Encoding::UTF_8.to_s)
+
+elementList = doc.search('.elementList')
+puts "There's #{elementList.length} quotes"
+
+elementList.each do |element|
+  quoteText = element.search('.quoteText').text.strip.split('//<![CDATA[')[0]
+
+  content = quoteText.split("\n")[0][1...-1]
+
+  # author = quoteText.split("\n")[2].tr(',', '').strip // old selector
+  author = element.search('span.authorOrTitle').text.strip.gsub(',', '')
+
+  author_img = element.search('a > img').empty? ? '' : element.search('a > img').attribute('src').value
+
+  # book = quoteText.split("\n")[4].nil? ? '' : quoteText.split("\n")[4].strip // old selector
+  book = element.search('a.authorOrTitle').text
+
+  quote = Quote.create({
+    content: content,
+    author: author,
+    author_img: author_img,
+    book: book
+  })
+end
