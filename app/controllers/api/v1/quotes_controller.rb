1cf59e1fbf26a8f0a300020f8d111ecf620b5b5a
Romain niam0r <romain@niamor.me>
2020-01-31 17:50:44 +0000

    WIP implement API

---
+++ app/controllers/api/v1/quotes_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class Api::V1::QuotesController < Api::V1::BaseController
+  before_action :set_quote, only: [:show]
+
+  # def pundit_user
+  #   User.find_by_other_means
+  # end
+
+  def index
+    @quotes = policy_scope(Quote)
+  end
+
+  def show; end
+
+  private
+
+  def set_quote
+    @quote = Quote.find(params[:id])
+    # authorize @quote
+  end
+end
