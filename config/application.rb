7226f2aa0bcd7d015996c0aa9872e9af55e61f68
Romain niam0r <romain@niamor.me>
2020-02-03 10:47:44 +0000

    Add rspec

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,9 @@ module GoodreadsQuotes
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.test_framework :rspec
+      g.integration_tool :rspec
+    end
   end
 end
