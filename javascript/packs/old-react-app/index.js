29b048181f0c9b98192b37c433c6d495108c5e6e
Romain niam0r <romain@niamor.me>
2020-02-04 13:16:00 +0000

    Clean up and WIP adding initialState

--- javascript/packs/old-react-app/index.js
+++ javascript/packs/old-react-app/index.js
@@ -1,7 +1,3 @@
-// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
-// of the page.
-
 // external modules
 import React from 'react'
 import ReactDOM from 'react-dom'
@@ -18,10 +14,6 @@ import QuotesShow from './containers/quotes_show';
 
 // state and reducers
 import quotesReducer from './reducers/quotes_reducer';
-
-// const initialState = {
-//   quotes: []
-// };
 
 const reducers = combineReducers({
   quotes: quotesReducer
