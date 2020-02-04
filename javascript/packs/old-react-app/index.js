9269efaf6b2837af284be2b2185f8a6ae6cedac5
Romain niam0r <romain@niamor.me>
2020-02-04 16:45:07 +0000

    Rename old-react-app

--- javascript/packs/old-react-app/index.js
+++ javascript/packs/old-react-app/index.js
@@ -5,10 +5,9 @@ import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import reduxPromise from 'redux-promise';
 import logger from 'redux-logger';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Router, Route, Switch } from 'react-router-dom';
-// import { createHistory as history } from 'history/createBrowserHistory';
-require("history").createBrowserHistory;
+import { createHistory as history } from 'history/createBrowserHistory';
 
 // internal modules
 import QuotesIndex from './containers/quotes_index';
