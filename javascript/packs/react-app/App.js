e261799fc8ea9ac99cfa8cef6f0e7e0836e82c05
Romain niam0r <romain@niamor.me>
2020-02-07 14:53:38 +0000

    Cleanup

--- javascript/packs/react-app/App.js
+++ javascript/packs/react-app/App.js
@@ -1,10 +1,7 @@
-import Example from "./Example";
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import QuotesList from "./QuotesList";
 import QuotesShow from "./QuotesShow";
-
-import getQuotes from './getQuotes';
 
 export default function App() {
   return (
