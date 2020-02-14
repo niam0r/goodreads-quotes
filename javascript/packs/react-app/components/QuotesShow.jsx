d36bd3bf8d8bcacd9d734a77ac3a33b64b84bfce
Romain niam0r <romain@niamor.me>
2020-02-14 15:52:32 +0000

    WIP implement previous/next quote link

--- javascript/packs/react-app/components/QuotesShow.jsx
+++ javascript/packs/react-app/components/QuotesShow.jsx
@@ -9,6 +9,10 @@ export default function QuotesShow() {
   const { id }  =  useParams();
   const { quotes } = useContext(QuotesContext);
   const [ quote, setQuote ] = useState({});
+  // what if previous or next quote is undefined?
+  const previousId = (parseInt(id) - 1).toString();
+  const nextId = (parseInt(id) + 1).toString();
+
 
   useEffect(() => {
     const quoteFromContext = quotes.find(quote => quote.id === parseInt(id));
@@ -17,13 +21,28 @@ export default function QuotesShow() {
   
   return ( 
     <div className='container'>
+
       <div style={{'text-align': 'center'}}>
         <Link to='/quotes'>
           <i className="fas fa-long-arrow-alt-left"></i>
           Back to all quotes
         </Link>
       </div>
+
       <Quote quote={quote}/>
+      
+      <div style={{'text-align': 'center'}}>
+        <Link to={'/quotes/' + previousId}>
+          <i class="fas fa-chevron-left"></i> Previous
+        </Link>
+      </div>
+
+      <div style={{'text-align': 'center'}}>
+        <Link to={'/quotes/' + nextId}>
+          <i class="fas fa-chevron-right"></i> Next
+        </Link>
+      </div>
+
     </div>
   );
 }
