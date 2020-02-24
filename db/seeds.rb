854bdebef21718253ef9970f0053f8659942f366
Romain niam0r <romain@niamor.me>
2020-02-24 14:43:27 +0000

    Implement simple RSS parser

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,40 @@
 # frozen_string_literal: true
 
-require 'open-uri'
+# require 'open-uri'
 
-Quote.destroy_all
-puts 'Destroyed all quotes'
+# Quote.destroy_all
+# puts 'Destroyed all quotes'
 
-url = 'https://www.goodreads.com/quotes/list/61714489-romain-niam0r?utf8=%E2%9C%93&sort=date_added'
-# url = "./app/my_quotes.html"
-doc = Nokogiri::HTML(open(url), nil, Encoding::UTF_8.to_s)
+# url = 'https://www.goodreads.com/quotes/list_rss/61714489-romain-niam0r'
+# # url = "./app/my_quotes.html"
+# doc = Nokogiri::HTML(open(url), nil, Encoding::UTF_8.to_s)
 
-element_list = doc.search('.elementList')
-puts "There's #{element_list.length} quotes"
 
-element_list.each do |element|
-  quote_text = element.search('.quoteText').text.strip.split('//<![CDATA[')[0]
+# ???.each do |element|
 
-  content = quote_text.split("\n")[0][1...-1]
+#   content =
+#   author =
+#   author_img =
+#   book =
 
-  # author = quote_text.split("\n")[2].tr(',', '').strip // old selector
-  author = element.search('span.authorOrTitle').text.strip.gsub(',', '')
+#   Quote.create(
+#     content: content,
+#     author: author,
+#     author_img: author_img,
+#     book: book
+#     # TODO: link: link to book on goodreads
+#   )
+# end
 
-  author_img = element.search('a > img').empty? ? '' : element.search('a > img').attribute('src').value
+require 'rss'
+require 'httparty'
 
-  # book = quote_text.split("\n")[4].nil? ? '' : quote_text.split("\n")[4].strip // old selector
-  book = element.search('a.authorOrTitle').text
+response = HTTParty.get('https://www.goodreads.com/quotes/list_rss/61714489-romain-niam0r')
 
-  Quote.create(
-    content: content,
-    author: author,
-    author_img: author_img,
-    book: book
-    # TODO: link: link to book on goodreads
-  )
+feed = RSS::Parser.parse response.body
+
+feed.items.each do |item|
+  puts '--------------------------------------------------------------------------------'
+  puts item.description
+  puts '--------------------------------------------------------------------------------'
 end
